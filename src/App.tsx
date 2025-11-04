11c523e02a364736e6acb944be3f6f81e881052e
Otito <urphasin@gmail.com>
2025-11-03 22:44:04 -0700

    first frontend commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="text-center p-6">
+      <h1 className="text-3xl font-bold text-blue-500">
+        Pet Matcher 🐶🐱
+      </h1>      
+    </div>
+  );
+}
+
+export default App
